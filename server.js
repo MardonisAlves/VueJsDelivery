3a4c8132f8382bcda49d0310ec47a7c18f948cdd
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-07-03 23:53:27 -0300

    pwa

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get(/.*/ , function(req , res) {
   res.sendfile(__dirname + "/dist/index.html");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
