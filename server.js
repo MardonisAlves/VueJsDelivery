b40810bc0179db80b3059f386bed858b4444b900
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-07-05 20:10:01 -0300

    imges-static

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const serveStatic = require('serve-static');
 let app = express();
 app.use(serveStatic(__dirname + "/dist/"));
 
+
+app.get('/', function(req , res) {
+  req.sendfile(__dirname + "/dist/index.html");
+});
+
 app.get(/.*/ , function(req , res) {
   res.sendfile(__dirname + "/dist/index.html");
 });
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,84 +1,34 @@
 <template>
   <div>
-  <b-container fluid>
+  <b-container>
   <b-row align-h="around">
-  <b-card
-     title="Calabresa"
-     img-src="https://picsum.photos/600/300/?image=21"
-     img-alt="Image"
-     img-top
-     tag="article"
-     style="max-width: 10rem;"
-     class="mb-2">
-     <b-card-text>
-       <p>M Valor 20.50</p>
-       <p>G Valor 22.50</p>
-     </b-card-text>
-
-     <b-button href="#" variant="primary">+</b-button>
-   </b-card>
-
-   <b-card
-      title="Card Title"
-      img-src="https://picsum.photos/600/300/?image=20"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 10rem;"
-      class="mb-2">
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
-      </b-card-text>
-
-      <b-button href="#" variant="primary">+</b-button>
-    </b-card>
-   <b-card
-      title="Card Title"
-      img-src="https://picsum.photos/600/300/?image=22"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 10rem;"
-      class="mb-2">
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
-      </b-card-text>
-
-      <b-button href="#" variant="primary">+</b-button>
-    </b-card>
-
-
-    <b-card
-       title="Card Title"
-       img-src="https://picsum.photos/600/300/?image=23"
-       img-alt="Image"
-       img-top
-       tag="article"
-       style="max-width: 10rem;"
-       class="mb-2">
+    <b-card-group deck>
+     <b-card title="Calabresa" img-src="static/images/calabresa.png" img-alt="Card image" img-top>
        <b-card-text>
-         Some quick example text to build on the card title and make up the bulk of the card's content.
-       </b-card-text>
-
-       <b-button href="#" variant="primary">+</b-button>
-     </b-card>
-
-
-     <b-card
-        title="Card Title"
-        img-src="https://picsum.photos/600/300/?image=24"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 10rem;"
-        class="mb-2">
-        <b-card-text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </b-card-text>
-
-        <b-button href="#" variant="primary">+</b-button>
+         Some quick example text to build on the card and make up the bulk of the card's content.
+      </b-card-text>
       </b-card>
-    </b-row>
+
+      <b-card title="Calabresa" img-src="static/images/calabresa.png" img-alt="Card image" img-top>
+        <b-card-text>
+          Some quick example text to build on the card and make up the bulk of the card's content.
+       </b-card-text>
+       </b-card>
+
+       <b-card title="Calabresa" img-src="static/images/calabresa.png" img-alt="Card image" img-top>
+         <b-card-text>
+           Some quick example text to build on the card and make up the bulk of the card's content.
+        </b-card-text>
+        </b-card>
+
+        <b-card title="Calabresa" img-src="static/images/calabresa.png" img-alt="Card image" img-top>
+          <b-card-text>
+            Some quick example text to build on the card and make up the bulk of the card's content.
+         </b-card-text>
+       </b-card>
+
+  </b-card-group>
+  </b-row>
   </b-container>
     </div>
 
@@ -91,11 +41,12 @@ export default {
 <style>
 .card{
   border: 0px;
-}
-.card{
   margin-top:80px;
 }
+.card-title{
+  font-size:12px;
+}
 .card-text{
-  font-size: 12px;
+  font-size: 10px;
 }
 </style>
